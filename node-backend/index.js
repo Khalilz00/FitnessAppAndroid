f60d3b34288f0dc95e290ae9f7fc386861f9dfba
vinc3yz <125292473+vinc3yz@users.noreply.github.com>
2024-09-15 16:27:35 +0200

    add base android project

--- node-backend/index.js
+++ node-backend/index.js
@@ -11,6 +11,12 @@ async function getSessions() {
   return sessions
 }
 
+async function getExcercises() {
+  const sql = postgres('postgres://fitnessapp:test@localhost:5432/fitness')
+  const excercises = await sql`SELECT * FROM exercise`
+  // Convert to json and return
+  return excercises
+}
 //const sql = postgres('postgres://postgres:password@localhost:5432/postgres')
 
 app.get('/', (req, res) => {
@@ -20,6 +26,12 @@ app.get('/', (req, res) => {
 app.get('/get-sessions', (req, res) => {
   getSessions().then((sessions) => {
     res.json(sessions)
+  })
+})
+
+app.get('/get-excercises', (req, res) => {
+  getSessions().then((excercises) => {
+    res.json(excercises)
   })
 })
 
