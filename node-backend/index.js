c7ce689b321cfd9e1d20a09749cbf915374012fc
Khalil ZANZOURI <khalilzanzouri@icloud.com>
2024-09-15 18:01:00 +0200

    Backend test

--- node-backend/index.js
+++ node-backend/index.js
@@ -2,14 +2,31 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const postgres = require('postgres')
+
+async function getSessions() {
+  const sql = postgres('postgres://fitnessapp:test@localhost:5432/fitness')
+  const sessions = await sql`SELECT * FROM exercise`
+  // Convert to json and return
+  return sessions
+}
+
+//const sql = postgres('postgres://postgres:password@localhost:5432/postgres')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/get-sessions', (req, res) => {
-    res.send('name id blabla!')
+  getSessions().then((sessions) => {
+    res.json(sessions)
+  })
 })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
