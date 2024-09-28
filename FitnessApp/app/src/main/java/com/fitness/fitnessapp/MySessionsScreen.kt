1af43af85b154a2078442e80c74337be92abe62c
vinc3yz <125292473+vinc3yz@users.noreply.github.com>
2024-09-28 14:21:07 +0200

    add my sessions screen base

---
+++ FitnessApp/app/src/main/java/com/fitness/fitnessapp/MySessionsScreen.kt
@@ -0,0 +1,9 @@
+package com.fitness.fitnessapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MySessionsScreen(navController: NavController) {
+
+}
